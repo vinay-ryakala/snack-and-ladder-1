985a692a04051fadc8c68dd00e29e8cd5991d32f
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-17 11:50:00 +0530

    adding lader and snake to sanke and ladder problem UC3

--- snackLadder.sh
+++ snackLadder.sh
@@ -8,10 +8,35 @@ function diceRoll()
 
 numberofPlayer=1
 position=0
-endPosition=100
-startPosition=0
-
+ENDPOSITION=100
+STARTPOSITION=0
+NOPLAY=0
+LADDER=1
+SNAKE=2
 echo "welcome to snake and ladder"
 
-diceRoll
+roll=$( diceRoll )
+checkOptions=$((RANDOM%3))
+
+case $checkOptions in
+	$NOPLAY)
+	position=$position
+	;;
+	$LADDER)
+	if [ $(($position+$roll)) -gt $ENDPOSITION ]
+	then
+	position=$ENDPOSITION
+	else
+	position=$(($position+$roll))
+	fi
+	;;
+	SNAKE)
+	if [ $(($position-$roll)) -lt $STARTPOSITION ]
+	then
+	position=$STARTPOSITION
+	else
+	position=$(($poition-$roll))
+	fi
+	;;
+esac
 
