5bca0f03caeb32b41ea8cb9a09b0dd0efb51ac63
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-17 10:24:50 +0530

    adding UC1 snake and ladder

--- snackLadder.sh
+++ snackLadder.sh
@@ -1,1 +1,17 @@
 #!/bin/bash -x
+
+function diceRoll()
+{
+roll=$((RANDOM%6+1))
+echo "$roll"
+}
+
+position=0
+endPosition=100
+
+while [ $position -ne $endPosition ]
+do 
+	rollingDice="$( diceRoll )"
+
+	position=$(( $position +$rollingDice )) 
+done
